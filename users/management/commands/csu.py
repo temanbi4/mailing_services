057d19f9fb4e976a12f0ddaf8b46d506477f0e22
temanbi4 <temanbi4@gmail.com>
2023-10-16 21:18:34 +0300

    created app users

---
+++ users/management/commands/csu.py
@@ -0,0 +1,17 @@
+from django.core.management import BaseCommand
+
+from users.models import User
+
+
+class Command(BaseCommand):
+    """Кастомная команда для создания суперпользователя"""
+
+    def handle(self, *args, **kwargs):
+        user = User.objects.create(email='admin@admin.admin',
+                                   first_name='admin',
+                                   last_name='adminos',
+                                   is_staff=True,
+                                   is_superuser=True)
+
+        user.set_password('369369')
+        user.save()
